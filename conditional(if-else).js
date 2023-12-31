07f788e9aafccaf68a9640b1bc020d4da89d2fd9
Huzaifa Shoaib <huzaifachughtai2001@gmail.com>
2023-12-31 14:53:31 +0500

    deleted

--- conditional(if-else).js
+++
@@ -1,9 +0,0 @@
-const result = 30;
-
-// using the if-else statement below
-
-if (result > 40) {
-  console.log("Congratulations! You passed the test.");
-} else {
-  console.log("You did not pass the test.");
-}
