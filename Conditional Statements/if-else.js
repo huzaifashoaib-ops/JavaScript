ae90c6b2f6fc08434a595f50fb88c24b6be648ff
Huzaifa Shoaib <huzaifachughtai2001@gmail.com>
2023-12-31 15:01:00 +0500

    The if-else statement

--- Conditional Statements/if-else.js
+++ Conditional Statements/if-else.js
@@ -1,6 +1,6 @@
 const result = 30;
 
-// using the if-else statement below
+// experiment with the code by making some changes and see how the changes affect the code
 
 if (result > 40) {
   console.log("Congratulations! You passed the test.");
