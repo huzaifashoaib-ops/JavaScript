45c1fd01a2001f4783c1927b3c1cc236f47c164d
Huzaifa Shoaib <huzaifachughtai2001@gmail.com>
2023-12-31 15:55:08 +0500

    The for loop

---
+++ Loops/for loop.js
@@ -0,0 +1,13 @@
+// experiment with the code by making some changes and see how the changes affect the code
+
+// loop with decrement
+for (let i = 10; i > 0; i--) {
+  console.log(i);
+}
+console.log("Happy New Year!\n");
+
+// loop with increment
+for (let i = 0; i <= 3; i++) {
+  console.log(i);
+}
+console.log("Go!");
