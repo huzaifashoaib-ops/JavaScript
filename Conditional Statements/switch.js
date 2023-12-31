8a913a3c49cc56e28452198e7cb7d80931a3964e
Huzaifa Shoaib <huzaifachughtai2001@gmail.com>
2023-12-31 15:00:34 +0500

    The Switch Statement

---
+++ Conditional Statements/switch.js
@@ -0,0 +1,18 @@
+const place = "first";
+
+// experiment with the code by making some changes and see how the changes affect the code
+
+switch (place) {
+  case "first":
+    console.log("Gold");
+    break;
+  case "second":
+    console.log("Silver");
+    break;
+  case "third":
+    console.log("Bronze");
+    break;
+  default:
+    console.log("No Medal.");
+    break;
+}
