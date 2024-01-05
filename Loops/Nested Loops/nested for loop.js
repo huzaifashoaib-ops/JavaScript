f5a191284351b48c572f41ae3df9e3897c1f4210
Huzaifa Shoaib <huzaifachughtai2001@gmail.com>
2024-01-05 09:10:18 +0500

    Nested For Loop

---
+++ Loops/Nested Loops/nested for loop.js
@@ -0,0 +1,7 @@
+// experiment with the code by making some changes and see how the changes affect the code
+for (let i = 2023; i < 2025; i++) {
+  console.log(i);
+  for (let j = 6; j < 9; j++) {
+    console.log("--------", j);
+  }
+}
